01a0a85c6d258eeb9704bf50e9e266679d9b9335
Adam Kahmann <akahmann93@gmail.com>
2018-06-11 14:42:47 -0600

    Initial commit

---
+++ android/src/com/kahmann/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.kahmann.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kahmann.game.LaserChase;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LaserChase(), config);
+	}
+}
---
+++ desktop/src/com/kahmann/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kahmann.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kahmann.game.LaserChase;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LaserChase(), config);
+	}
+}
