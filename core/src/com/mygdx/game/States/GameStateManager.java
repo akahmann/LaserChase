35eee070f96ad64dfa4fbacf863a8714a0e8bc83
Adam Kahmann <akahmann93@gmail.com>
2018-07-06 10:47:32 -0600

    Javadoc comments for Game State Manager

--- core/src/com/mygdx/game/States/GameStateManager.java
+++ core/src/com/mygdx/game/States/GameStateManager.java
@@ -8,19 +8,33 @@ public class GameStateManager {
 
     private Stack<State> states;
 
+    /**
+     * Constructs a GameState Manager that contains a stack of states. States represent what state
+     * the game is currently in. For example there could be a menu state, a play state, or a
+     * gameover state. It is a stack because once a state is over it will go back to the previous
+     * state.
+     */
     public GameStateManager(){
         states = new Stack<State>();
-
     }
 
+    /**
+     * Adds a new state to the stack. The front of stack represents the current state displayed
+     * and active.
+     *
+     * @param state A new state to be displayed and active.
+     */
     public void push(State state){
         states.push(state);
-
     }
 
+    /**
+     * Exit out of the current state.
+     */
     public void pop(){
         states.pop().dispose();
     }
+
 
     public void set(State state){
         states.pop(); // might need .dispose() later
