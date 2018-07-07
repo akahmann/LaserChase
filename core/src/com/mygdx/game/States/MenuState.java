889d0857ba7e7e7ee7d31f4cf4ced58d8b2518af
Justin Bengtson <justinbengtson@icloud.com>
2018-07-07 11:55:02 -0600

    Made it so play button changes menu state to play state

--- core/src/com/mygdx/game/States/MenuState.java
+++ core/src/com/mygdx/game/States/MenuState.java
@@ -3,26 +3,38 @@ package com.mygdx.game.States;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.Rectangle;
 
 public class MenuState extends State {
 
     private Texture background;
     private Texture playBtn;
+    private Rectangle btnBounds;
+
 
 
     public MenuState(GameStateManager gsm) {
         super(gsm);
         background = new Texture("spr_cat.png");
         playBtn = new Texture("PlayBtn.png");
+        btnBounds = new Rectangle(Gdx.graphics.getWidth() / 2 - (playBtn.getWidth() / 2), Gdx.graphics.getHeight() / 2, 600, 260);
     }
 
     @Override
     public void handleInput() {
         if(Gdx.input.justTouched()){
-            gsm.set(new PlayState(gsm));
+            Vector3 touchPosition = new Vector3();
+            touchPosition.set(Gdx.input.getX(), Gdx.graphics.getHeight() -  Gdx.input.getY(), 0);
+            Rectangle touchPosRec = new Rectangle(touchPosition.x, touchPosition.y, 1, 1);
+
+           // laser.teleport(laserPosition);
+            if(touchPosRec.overlaps(btnBounds))
+                gsm.set(new PlayState(gsm));
         }
 
     }
+
 
     @Override
     public void update(float dt) {
